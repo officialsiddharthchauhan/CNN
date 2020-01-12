b9461c94831c2a4f43e02d656156ad245c04fb60
siddharth chauhan <43745848+officialsiddharthchauhan@users.noreply.github.com>
2020-01-12 20:55:22 +0530

    Add files via upload

---
+++ catsanddogsbytransferlearning.py
@@ -0,0 +1,4 @@
+#Saving and loading models in tensorflow
+#source-https://colab.research.google.com/github/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l07c01_saving_and_loading_models.ipynb
+#Github-https://github.com/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l07c01_saving_and_loading_models.ipynb
+
